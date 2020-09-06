3e76e71d42d49491d7645d30901dd0001771e503
DO <as.sd.show@gmail.com>
2020-09-06 16:53:00 +0900

    2020. 09. 06 <1> 간단한 캐릭터 무빙 및 인게임 UI 구현 Shop UI 구현

---
+++ 2020_2_Project/Assets/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2d;
+    private Animator _animator;
+
+    private Vector2 _movePos;
+    private float _speed = 4;
+    
+    private void Awake()
+    {
+        _rigidbody2d = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+
+    public void Move(int _direction)
+    {
+        _animator.SetInteger("direction", _direction);
+        _animator.SetBool("move", true);
+        _movePos.x = _direction;
+    }
+
+    public void Idle()
+    {
+        _animator.SetBool("move", false);
+        _movePos = Vector2.zero;
+    }
+
+    public void Jump()
+    {
+        _animator.SetTrigger("jump");
+        _rigidbody2d.velocity = new Vector2(0, 10);
+    }
+
+    private void Update()
+    {
+        _rigidbody2d.transform.Translate(_movePos.normalized * Time.deltaTime * _speed);
+    }
+}
