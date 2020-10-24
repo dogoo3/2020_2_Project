a641b157436f01174eff2a44c41796d473d2dda0
DO <as.sd.show@gmail.com>
2020-10-25 02:55:23 +0900

    2020. 10. 25 <2> Goblin Ai System 재편집(점프 높낮이 폭을 조정해봄)

--- 2020_2_Project/Assets/Scripts/Objects/Enemys/Goblin.cs
+++ 2020_2_Project/Assets/Scripts/Objects/Enemys/Goblin.cs
@@ -17,7 +17,6 @@ public class Goblin : MonoBehaviour
     private bool _isJump; // 점프할 때
     private bool _isIdle; // 가만히 서 있을 때
     private float _changedirTime; // 시점 변환 시간을 계산하는 변수
-    private float _changeatkTime; // 공격 쿨타임
     private float _changeatkedTime; // 피격 후 경과 시간
     private bool _isattacked; // 공격 당했을 때
     private bool _isattack; // 공격했을 때
@@ -49,7 +48,6 @@ public class Goblin : MonoBehaviour
     {
         // Initialize
         _changedirTime = 0;
-        _changeatkTime = 0;
         _direction = Vector2.right;
         _animator.SetFloat("direction", _direction.x);
         SetPatrolTime();
@@ -60,51 +58,37 @@ public class Goblin : MonoBehaviour
         if (_isDetectStart)
         {
             _changedirTime += Time.deltaTime;
+            if (_isattacked)  // 피격 상태일 때
+            {
+                _changeatkedTime += Time.deltaTime;
+                if (_changeatkedTime > 2.0f) // 피격 후 2초 경과 시 평상시 walk로 돌아간다.
+                {
+                    _isattacked = false; 
+                    _animator.SetBool("attacked", _isattacked);
+                    _enemy.SetDef(_isattacked);
+                }
+            }
 
             if (!_isdetect) // 플레이어 탐색 상태
             {
-                if (!_isIdle && !_isattack) // 무브상태일때(아이들이 아닐 경우), 혹은 비공격상태일때
+                if(!_isIdle && !_isattack)
                     transform.Translate(_direction * moveSpeed * Time.deltaTime);
-                if (_changedirTime > _patrolTime) // 상태변환 시간
-                    ChangeMotion(); // 모션변경
-                if (!_isJump) // 평지를 걷고 있을 경우
-                    DetectGround(); // 지속적인 Raycast 탐색
-
-                if (_isattack) // 고블린이 공격을 했을 때 일정시간동안 공격할 수 없게한다.
-                {
-                    _changeatkTime += Time.deltaTime;
-                    if (_changeatkTime > 3.0f)
-                        _isattack = false;
-                }
-
-                if (_isattacked) // 고블린이 공격을 당했을 때부터 일정시간 경과하면 방패 모드를 해제한다.
-                {
-                    _changeatkedTime += Time.deltaTime;
-                    if (_changeatkedTime > 2.0f) // 공격당한지 2초가 넘게 되면
-                    {
-                        _isattacked = false;
-                        _animator.SetBool("attacked", _isattacked);
-                        _enemy.SetDef(_isattacked);
-                    }
-                }
+                if(_changedirTime > _patrolTime)
+                    ChangeMotion();
+                if(!_isJump && !_isIdle)
+                    DetectGround();
 
                 if (Player.instance != null)
                 {
-                    if(!Player.instance.CheckAttacked())
+                    if (!Player.instance.CheckAttacked()) // 플레이어가 무적상태인지 체크 
                     {
-                        if (!_isattack) // 고블린이 공격이 가능한 상태여야 공격할수있음!
+                        if (!_isattack) // 고블린이 공격이 가능한 상태여야 감지 가능!
                         {
-                            if (Vector2.Distance(Player.instance.transform.position, transform.position) < rayLength) // 시점 내에 플레이어가 감지될경우
+                            if (Vector2.Distance(Player.instance.transform.position, transform.position) < rayLength) // 플레이어가 Ray의 거리 내에 있는지
                             {
-                                _rayPlayer = Physics2D.Raycast(transform.position, _direction, rayLength, 1 << LayerMask.NameToLayer("Player"));
-
+                                _rayPlayer = Physics2D.Raycast(transform.position + Vector3.down * 1.4f, _direction, rayLength, 1 << LayerMask.NameToLayer("Player"));
                                 if (_rayPlayer.collider != null)
-                                {
-                                    _isdetect = true;
-                                    _animator.SetFloat("run", 1.5f);
-                                    _changedirTime = 0;
-                                    SetPatrolTime();
-                                }
+                                    ChangeDetectMode(true, 1.5f); // 플레이어 감지 모드로 변경
                             }
                         }
                     }
@@ -115,26 +99,10 @@ public class Goblin : MonoBehaviour
                 transform.Translate(_direction * runSpeed * Time.deltaTime);
 
                 if (_changedirTime > _patrolTime)
-                {
-                    _isdetect = false;
-                    _animator.SetFloat("run", 1.0f);
-                    _changedirTime = 0;
-                    SetPatrolTime();
-                }
-                _rayPlayer = Physics2D.Raycast(transform.position + (Vector3.down * 0.5f), _direction, 4.0f, 1 << LayerMask.NameToLayer("Ground"));
-                if (_rayPlayer.collider == null) // 앞이 안 막혀있고
-                {
-                    _rayPlayer = Physics2D.Raycast(transform.position + (Vector3)_direction + (Vector3.down * 1.5f), Vector2.down, 2f, 1 << LayerMask.NameToLayer("Ground"));
-                    if (_rayPlayer.collider == null) // 진행방향에 브릿지가 없으면
-                    {
-                        // 점프한다
-                        if (!_isJump)
-                        {
-                            _rigidbody2d.velocity = new Vector2(0, jumpforce);
-                            _isJump = true;
-                        }
-                    }
-                }
+                    ChangeDetectMode(false, 1.0f);
+
+                if (!_isJump)
+                    DetectGround();
 
                 if (!Player.instance.CheckAttacked()) // 플레이어가 피격후 무적상태면 감지하지 않는다. (무적상태 : true)
                 {
@@ -142,10 +110,8 @@ public class Goblin : MonoBehaviour
                     {
                         Player.instance.Attacked(damage);
                         _animator.SetTrigger("attack");
-                        _animator.SetBool("idle", true);
                         _isdetect = false; // 플레이어 감지 해제
-                        _isattack = true; // 고블린이 공격모션을 취함. 이 이후로 n초동안 공격 및 감지 불가능.
-                        _changeatkTime = 0;
+                        _isattack = true; // 공격모션을 취하는 동안 이동할 수 없음.
                     }
                 }
             }
@@ -176,42 +142,59 @@ public class Goblin : MonoBehaviour
 
     private void DetectGround()
     {
-        _rayPlayer = Physics2D.Raycast(transform.position + (Vector3)_direction + (Vector3.down * 1.5f), Vector2.down, 2f, 1 << LayerMask.NameToLayer("Ground"));
-        Debug.DrawRay(transform.position + (Vector3)_direction + (Vector3.down * 1.5f), Vector2.down, Color.red, 0.1f);
-        if(_rayPlayer.collider == null) // 시선 앞에 땅이 없으면
+        _rayPlayer = Physics2D.Raycast(transform.position, _direction, 3.0f, 1 << LayerMask.NameToLayer("Wall")); // 벽을 감지해서
+        Debug.DrawRay(transform.position, _direction * 3.0f, Color.blue, 0.1f);
+        if (_rayPlayer.collider != null) // 벽을 감지하면 무조건 시점전환.
         {
-            _randAction = Random.Range(0, 2);
-            if (_randAction == 0) // 방향변경
+            ChangeDir();
+        }
+
+        _rayPlayer = Physics2D.Raycast(transform.position + (Vector3)_direction + (Vector3.down * 2.2f), Vector2.down, 0.5f, 1 << LayerMask.NameToLayer("Ground"));
+        Debug.DrawRay(transform.position + (Vector3)_direction + (Vector3.down * 2.2f), Vector2.down * 0.5f, Color.red, 0.1f);
+        if (_rayPlayer.collider == null) // 시선 앞에 땅이 없으면
+        {
+            _rayPlayer = Physics2D.Raycast(transform.position + Vector3.down * 1.6f, _direction, 2f, 1 << LayerMask.NameToLayer("Ground"));
+            Debug.DrawRay(transform.position + Vector3.down * 1.6f, _direction * 2f, Color.green, 1.0f);
+            if (_rayPlayer.collider != null) // 점프할 땅이 있으면
             {
-                ChangeDir();
+                _randAction = Random.Range(0, 2); // 50%의 확률로
+                if (_randAction == 0) // 점프한다
+                {
+                    _isJump = true;
+                    _rigidbody2d.velocity = new Vector2(0, jumpforce);
+                }
+                else // 방향전환한다
+                    ChangeDir();
+                return;
             }
-            else if (_randAction == 1) // 점프
-            {
-                _isJump = true;
-                _rigidbody2d.velocity = new Vector2(0, jumpforce);
-            }
-            else { } 
+        }
+        _rayPlayer = Physics2D.Raycast(transform.position + Vector3.down * 0.15f, _direction, 2.0f, 1 << LayerMask.NameToLayer("Ground"));
+        Debug.DrawRay(transform.position + Vector3.down*0.15f, _direction * 2.0f, Color.yellow, 0.1f);
+        if(_rayPlayer.collider != null)
+        {
+            _isJump = true;
+            _rigidbody2d.velocity = new Vector2(0, jumpforce * 1.5f);
         }
     }
 
     private void ChangeMotion()
     {
         _randAction = Random.Range(0, 3);
-        if (_randAction == 0) // 방향 변경
+        Debug.Log(_randAction);
+        switch (_randAction)
         {
-            ChangeDir();
-            _isIdle = false;
-            _animator.SetBool("idle", _isIdle);
-        }
-        else if (_randAction == 1) // 정지
-        {
-            _isIdle = true;
-            _animator.SetBool("idle", _isIdle);
-        }
-        else // 그대로 진행
-        {
-            _isIdle = false;
-            _animator.SetBool("idle", _isIdle);
+            case 0:
+                ChangeDir();
+                SetIdle(false);
+                break;
+            case 1:
+                _isJump = true;
+                SetIdle(false);
+                _rigidbody2d.velocity = new Vector2(0, jumpforce);
+                break;
+            case 2:
+                SetIdle(true);
+                break;
         }
         SetPatrolTime();
         _changedirTime = 0;
@@ -219,8 +202,22 @@ public class Goblin : MonoBehaviour
 
     private void ChangeDir()
     {
-        _direction.x *= -1;
+        _direction *= -1;
         _animator.SetFloat("direction", _direction.x);
+    }
+
+    private void ChangeDetectMode(bool _is, float _speed)
+    {
+        _isdetect = _is;
+        _animator.SetFloat("run", _speed);
+        _changedirTime = 0;
+        SetPatrolTime();
+    }
+
+    private void SetIdle(bool _is)
+    {
+        _isIdle = _is;
+        _animator.SetBool("idle", _isIdle);
     }
 
     private void SetPatrolTime()
@@ -231,6 +228,11 @@ public class Goblin : MonoBehaviour
     public void ActiveDeteteStart() // Animation Func, 모든 행동을 정지할 때 사용.
     {
         _isDetectStart = true;
+    }
+
+    public void AttackChange() // Animation Func
+    {
+        _isattack = false;
     }
 
     public void Delete() // Animation Func
