cbcb91f9aa043fe8c6f99cee44185a43f652f22d
DO <dogoo3@naver.com>
2021-04-14 18:54:05 +0900

    툴팁 버그 수정

---
+++ 2020_2_Project/Assets/Scripts/GameNoticePanel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GameNoticePanel : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private GameObject _noticeImage = default;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!_noticeImage.activeSelf)
+            _noticeImage.SetActive(true);
+    }}
